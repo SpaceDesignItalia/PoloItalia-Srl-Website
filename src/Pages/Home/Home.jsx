bb402c891ad4193df3228e114ebcf907206699f6
Francesco Roviello <frarovi04@gmail.com>
2024-01-17 13:16:02 +0100

    ProductsSection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import Partners from "./Partners";
 import CustomersAroundWorld from "./CustomersAroundWorld";
 import OurStrengths from "./OurStrengths";
+import Products from "./Products";
 
 export default function Home() {
   const { lang } = useParams();
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <Partners />
       <OurStrengths />
+      <Products />
       <CustomersAroundWorld />
     </>
   );
