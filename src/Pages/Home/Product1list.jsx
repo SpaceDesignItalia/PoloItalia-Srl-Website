8f8817f6126ae6438487f29a7b0107cf849df199
Francesco Roviello <frarovi04@gmail.com>
2024-01-19 22:12:42 +0100

    Products section

---
+++ src/Pages/Home/Product1list.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+const categories = [
+  {
+    name: "Lorem ipsum",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-collection-01.jpg",
+    imageAlt: "Office content 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+  },
+  {
+    name: "Lorem ipsum",
+    href: "#",
+    imageSrc:
+      "https://tailwindui.com/img/ecommerce-images/home-page-01-collection-02.jpg",
+    imageAlt: "Office content 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+  },
+];
+
+export default function Product1list() {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto max-w-xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Lorem ipsum
+        </h2>
+        <p className="mt-4 text-base text-gray-500">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+
+        <div className="mt-10 space-y-12 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:space-y-0">
+          {categories.map((category) => (
+            <a key={category.name} href={category.href} className="group block">
+              <div
+                aria-hidden="true"
+                className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg lg:aspect-h-6 lg:aspect-w-5 group-hover:opacity-75"
+              >
+                <img
+                  src={category.imageSrc}
+                  alt={category.imageAlt}
+                  className="h-full w-full object-cover object-center"
+                />
+              </div>
+              <h3 className="mt-4 text-base font-semibold text-gray-900">
+                {category.name}
+              </h3>
+              <p className="mt-2 text-sm text-gray-500">
+                {category.description}
+              </p>
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Home/Products.jsx
+++ src/Pages/Home/Products.jsx
@@ -1,96 +1,49 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import Product1list from "./Product1list";
 
 export default function Products() {
   const { t } = useTranslation();
   return (
-    <div className="bg-white max-w-screen-2xl mx-auto py-24 sm:py-32">
-      <section aria-labelledby="features-heading" className="relative">
-        <div className="aspect-h-2 aspect-w-3 overflow-hidden sm:aspect-w-5 lg:aspect-none lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-16">
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/confirmation-page-01-hero.jpg"
-            alt="Office content 1"
-            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-          />
-        </div>
+    <>
+      <section>
+        <div className="gap-16 items-center py-2 px-4 md:mx-auto md:max-w-screen-xl flex flex-col md:flex-row lg:justify-between lg:py-4 lg:px-6">
+          <div className="md:w-1/2">
+            <h2 className="mb-4 text-4xl font-bold text-gray-900 dark:text-white">
+              {t("Home.Product1")}
+            </h2>
+            <p className="md:text-lg mb-4">{t("Home.Product1desc")}</p>
+          </div>
 
-        <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 sm:pb-32 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:pt-32">
-          <div className="lg:col-start-2">
-            <p className="mt-4 text-4xl font-bold tracking-tight text-gray-900">
-              {t("Home.OurStrength")}
-            </p>
-            <p className="mt-8 text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <dl className="justify-center text-center items-center mt-16 grid grid-cols-1 gap-x-8 gap-y-10 text-sm sm:grid-cols-1">
-              <div>
-                <dt className="font-medium text-gray-900">Lorem ipsum</dt>
-                <dd className="mt-2 text-gray-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </dd>
-              </div>
-              <div>
-                <dt className="font-medium text-gray-900">Lorem ipsum</dt>
-                <dd className="mt-2 text-gray-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </dd>
-              </div>
-              <div>
-                <dt className="font-medium text-gray-900">Lorem ipsum</dt>
-                <dd className="mt-2 text-gray-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </dd>
-              </div>
-            </dl>
+          <div className="mt-8 md:w-1/2">
+            <img
+              className="mt-4 w-full lg:mt-10"
+              src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+              alt="office content 2"
+            />
+          </div>
+        </div>
+        <Product1list />
+      </section>
+
+      <section>
+        <div className="gap-16 items-center py-8 px-4 md:mx-auto md:max-w-screen-xl flex flex-col md:flex-row-reverse lg:justify-between lg:px-6">
+          <div className="md:w-1/2">
+            <h2 className="mb-4 text-4xl font-bold text-gray-900 dark:text-white">
+              {t("Home.Product2")}
+            </h2>
+            <p className="md:text-lg mb-4">{t("Home.Product2desc")}</p>
+          </div>
+          <div className="mt-8 md:w-1/2">
+            <img
+              className="mt-4 w-full lg:mt-10"
+              style={{ width: "100%", height: "auto" }}
+              src="https://images.unsplash.com/photo-1521737711867-e3b97375f902?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"
+              alt="office content 2"
+            />
           </div>
         </div>
       </section>
-      <section aria-labelledby="features-heading" className="relative">
-        <div className="aspect-h-2 aspect-w-3 overflow-hidden sm:aspect-w-5 lg:aspect-none lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-16">
-          <img
-            src="https://tailwindui.com/img/ecommerce-images/confirmation-page-01-hero.jpg"
-            alt="Office content 1"
-            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-          />
-        </div>
-
-        <div className="mx-auto max-w-2xl px-4 pb-24 pt-16 sm:px-6 sm:pb-32 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:pt-32">
-          <div className="lg:col-start-2">
-            <p className="mt-4 text-4xl font-bold tracking-tight text-gray-900">
-              {t("Home.OurStrength")}
-            </p>
-            <p className="mt-8 text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <dl className="justify-center text-center items-center mt-16 grid grid-cols-1 gap-x-8 gap-y-10 text-sm sm:grid-cols-1">
-              <div>
-                <dt className="font-medium text-gray-900">Lorem ipsum</dt>
-                <dd className="mt-2 text-gray-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </dd>
-              </div>
-              <div>
-                <dt className="font-medium text-gray-900">Lorem ipsum</dt>
-                <dd className="mt-2 text-gray-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </dd>
-              </div>
-              <div>
-                <dt className="font-medium text-gray-900">Lorem ipsum</dt>
-                <dd className="mt-2 text-gray-500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                </dd>
-              </div>
-            </dl>
-          </div>
-        </div>
-      </section>
-    </div>
+    </>
   );
 }
